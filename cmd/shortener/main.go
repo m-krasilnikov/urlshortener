6fc6f24dacc2ca448c7f1af4d103786aebc13f8f
Mike <mike@MacBook-Air-Mike.local>
2026-09-26 19:40:17 +0300

    (feature): Add logging for GET and POST

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,22 +4,36 @@ import (
 	"log"
 	"net/http"
 
+	"go.uber.org/zap"
+
 	"github.com/m-krasilnikov/urlshortener/internal/app"
 	"github.com/m-krasilnikov/urlshortener/internal/config"
+	"github.com/m-krasilnikov/urlshortener/internal/middleware"
 	"github.com/m-krasilnikov/urlshortener/internal/storage"
 )
 
 func main() {
 	cfg := config.New()
 	st := storage.NewMemoryStorage()
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Sync()
+
 	router := app.NewRouter(st, cfg.BaseURL)
+
+	handler := middleware.WithLogging(logger)(router)
+
 	log.Printf(
 		"server started at http://%s",
 		cfg.ServerAddress,
 	)
+
 	if err := http.ListenAndServe(
 		cfg.ServerAddress,
-		router,
+		handler,
 	); err != nil {
 		log.Fatal(err)
 	}
